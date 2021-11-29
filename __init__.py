cd9e0dd8bb5a79b0c6a4c6cc64b48941d4d49858
DavidHazaniInt <david.hazani@intsights.com>
2021-11-29 15:41:17 +0200

    New version of intsights connector

---
+++ __init__.py
@@ -0,0 +1,12 @@
+# File: __init__.py
+#
+# Copyright (c) 2019-2021 IntSights Cyber Intelligence Ltd.
+#
+# This unpublished material is proprietary to IntSights.
+# All rights reserved. The methods and
+# techniques described herein are considered trade secrets
+# and/or confidential. Reproduction or distribution, in whole
+# or in part, is forbidden except by express written permission
+# of IntSights.
+#
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
